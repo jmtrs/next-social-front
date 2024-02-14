5b3039ce48518bc81740e8e98ca036a2c1a432bc
jmtrs <jmtorres15@gmail.com>
2024-02-14 18:39:41 +0100

    feat(NotFoundPage): NotFoundPage with test creation

--- tests/Pages/NotFoundPage.test.jsx
+++ tests/Pages/NotFoundPage.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import NotFoundPage from '../../src/Pages/NotFoundPage.jsx'
+import NotFoundPage from '../../src/pages/NotFoundPage.jsx'
 import { describe, expect, test } from '@jest/globals'
 
 describe('NotFoundPage Component', () => {
